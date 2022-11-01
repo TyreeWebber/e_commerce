cacc85978370d95edaa93ca7bfbbeb0823efd16f
Tyree Webber <webber.tyree@gmail.com>
2022-11-01 17:30:08 -0600

    updated placeholder description to be  literally placeholder description so kirsten isn't confused at pirateipsum later and I don't have to sound like a nerd explaining lorem/pirate ipsum to her in the future.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,9 +4,9 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-  { id: 1, name: 'Painting', description: "Man-of-war run a rig heave.", price: '$example', image: 'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png'},
-  { id: 2, name: 'Charm', description:"To go on account gunwalls galleon.", price: '$example', image: 'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png'},
-  { id: 3, name: 'Pendant', description :"Prow scuttle parrel provost Sail", price: '$example', image: 'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png'},
+  { id: 1, name: 'Painting', description: "Placeholder Description.", price: '$example', image: 'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png'},
+  { id: 2, name: 'Charm', description:"Placeholder Description.", price: '$example', image: 'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png'},
+  { id: 3, name: 'Pendant', description :"Placeholder Description.", price: '$example', image: 'https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png'},
 ];
 
 const Products = () => {
