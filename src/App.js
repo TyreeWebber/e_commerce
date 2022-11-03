e25184663c480eeed58e699119e7929756e6c857
Tyree Webber <webber.tyree@gmail.com>
2022-11-03 10:45:37 -0600

    implement commerce.js and hide public key in .env file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+//commerceJS
+import { commerce } from './lib/commerce';
 //components
 import { Products, Navbar } from './components';
+import Commerce from '@chec/commerce.js';
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+  
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Navbar />
