b6e30a13a85f7694e1cdd693fe46c2b5304110ff
Tyree Webber <webber.tyree@gmail.com>
2023-01-01 13:43:21 -0700

    export checkout component via index.js and begin work on checkout component

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core';
 
 const Checkout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">Checkout</Typography>
+          <Stepper activeStep={0} className={classes.stepper}>
+            
+          </Stepper>
+        </Paper>
+      </main>
+    </>
   )
 }
 
