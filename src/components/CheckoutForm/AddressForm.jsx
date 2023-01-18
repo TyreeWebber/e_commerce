da7f211efe14c7d9587ac2fa0b071890f5daa2ab
Tyree Webber <webber.tyree@gmail.com>
2023-01-17 17:23:34 -0700

    Create working shipping address form in stepper, and fix broken controller

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
+import FormInput from './CustomTextField';
+
 const AddressForm = () => {
   const methods = useForm();
 
@@ -9,9 +11,14 @@ const AddressForm = () => {
     <>
       <Typography variant ="h6" gutterBottom>Shipping Address</Typography>
       <FormProvider {...methods}>
-        <form onSubmit={submit}>
+        <form onSubmit=''>
           <Grid container spacing={3}>
-
+            <FormInput required name='firstName' label='First Name' />
+            <FormInput required name='lastName' label='Last Name' />
+            <FormInput required name='address' label='Address' />
+            <FormInput required name='email' label='Email' />
+            <FormInput required name='city' label='City' />
+            <FormInput required name='postalcode' label='Postal Code' />
           </Grid>
         </form>
       </FormProvider>
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import { TextField, Grid, } from '@material-ui/core';
+import React from 'react';
+import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
 
-const FormInput = ({ name, label, required }) => {
-  const { control } = useFormContext();
 
-  return (
-    <Grid item xs={12} sm={6}>
-      <Controller
-        as={TextField}    
-        control={control} 
-        fullWidth
-        name={name} 
-        label={label}
-        required={required}
-      />
-    </Grid>
-  )
-}
+const FormInput = ({ name, label, required}) => {
+const { control } = useFormContext();
+   const isError = false;
 
-export default FormInput
+return (
+   <>
+         <Controller
+            control={control}
+            name={name}
+            render = {({ field})=> (
+                <TextField
+                    fullWidth
+                    label={label}
+                    required
+                />
+            )}
+         />
+   </>
+ );
+ }
+
+export default FormInput;
